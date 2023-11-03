0870932bb1ba45bb50c270818934c72183239b4a
Thitat Auareesuksakul <flux@thitat.net>
2023-11-03 19:44:52 +0900

    feat: add homepage notes and contact info

---
+++ src/lib/components/LinkButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let href: string | undefined = undefined;
+
+  let commonStyle = "p-2 transition-colors rounded-lg";
+</script>
+
+{#if href}
+  <a
+    class={`${commonStyle} bg-sky-100/50 text-sky-700 hover:bg-sky-100 active:text-sky-900 active:bg-sky-200`}
+    {href}><slot /></a
+  >
+{:else}
+  <span class={`${commonStyle} bg-neutral-100 text-neutral-500 cursor-default`}><slot /></span>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Under Construction</h1>
-<p>thitat.net is under construction.</p>
+<script lang="ts">
+  import LinkButton from "$lib/components/LinkButton.svelte";
+</script>
+
+<svelte:head>
+  <title>Home - thitat.net</title>
+</svelte:head>
+
+<div class="flex min-h-screen items-center justify-center p-3">
+  <div class="flex flex-col gap-4 w-full max-w-md">
+    <div class="text-center">
+      <h1 class="text-3xl font-bold mb-1">thitat.net</h1>
+      <p class="text-sm italic text-neutral-400">my website is under construction</p>
+    </div>
+
+    <div class="flex flex-col w-full p-2 rounded-lg border shadow-lg text-center">
+      <p class="text-xs text-neutral-400 mb-2">reach me on</p>
+      <div class="flex flex-col gap-1">
+        <LinkButton href="https://github.com/fluxth">GitHub</LinkButton>
+        <LinkButton href="https://linkedin.com/in/fluxth">LinkedIn</LinkButton>
+        <LinkButton>
+          flux<span class="at-prefix">thitat.net</span>
+        </LinkButton>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .at-prefix:before {
+    content: "@";
+  }
+</style>
